fd16a6bf41ebaac21e0ac025ebef608775296831
pamelaachirino <pamelaagustinachirino@gmail.com>
2023-03-27 22:41:25 -0300

    Creacion maqueta veterinaria

---
+++ administracion-veterinaria/src/components/Paciente.jsx
@@ -0,0 +1,24 @@
+
+export const Paciente = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Nombre: {' '}
+                <span className="font-normal normal-case">Hook</span>
+
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Propietario: {' '}
+                <span className="font-normal normal-case">Pame</span>
+
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Email: {' '}
+                <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Fecha Alta: {' '}
+                <span className="font-normal normal-case">26 de Marzo de 2023</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Síntomas: {' '}
+                <span className="font-normal normal-case">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</span>
+            </p>
+        </div>
+    )
+}
